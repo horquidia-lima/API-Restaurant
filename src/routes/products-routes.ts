c3d104bdf6f6283fd131e71bc5ff2edc45df732d
Horquidia <horquidia.a.lima@gmail.com>
2025-05-13 13:54:24 -0300

    continuacao da criacao da estrutura do projeto

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { ProductsController } from "../controllers/products-controller";
 
 const productsRoutes = Router();
+const productsController = new ProductsController()
 
-productsRoutes.get("/", () => {
-    
-})
+productsRoutes.get("/", productsController.index)
 
 export { productsRoutes }
