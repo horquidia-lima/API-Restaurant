c086352a957ae1074edd10f6c87ed966f3b36bef
Horquidia <horquidia.a.lima@gmail.com>
2025-05-13 21:49:03 -0300

    Validando dados do produto

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,9 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import {z} from 'zod'
 
 class ProductsController {
     async index(request: Request, response: Response, next: NextFunction) {
         try {
             
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async create(request: Request, response: Response, next: NextFunction) {
+        try {
+            const bodySchema = z.object({
+                name: z.string().trim().min(6),
+                price: z.number().gt(0, {message: 'O preço deve ser maior que 0'}),
+            })
+
+            const { name, price } = bodySchema.parse(request.body)
+
+            return response.status(201).json({ name, price })
+          
         } catch (error) {
             next(error)
         }
