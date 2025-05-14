9dbb50659494a33cc6fbe5251933bfebdc83e079
Horquidia <horquidia.a.lima@gmail.com>
2025-05-14 14:21:44 -0300

    Atualizando produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -38,6 +38,35 @@ class ProductsController {
             next(error)
         }
     }
+
+    async update(request: Request, response: Response, next: NextFunction) {
+        try {
+
+            const id = z
+                .string()
+                .transform((value) => Number(value))
+                .refine((value) => !isNaN(value), {message: "ID inválido"})
+                .parse(request.params.id)
+
+            const bodySchema = z.object({
+                name: z.string().trim().min(6),
+                price: z.number().gt(0),
+            })
+
+            const { name, price } = bodySchema.parse(request.body)
+
+            await knex<ProductRepository>('products').update({
+                name,
+                price,
+                update_at: knex.fn.now()
+            }).where({id})
+
+            return response.json()
+
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 export { ProductsController }
